6039a4f8fff1474fb99c03167e90200c005ae53b
Anthony Polka <anthonypolka@gmail.com>
2021-08-20 11:27:52 -0500

    Initial Commit

---
+++ National Parks/National_ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  National_ParksApp.swift
+//  National Parks
+//
+//  Created by Anthony Polka on 8/20/21.
+//
+
+import SwiftUI
+
+@main
+struct National_ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
